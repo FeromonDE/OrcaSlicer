db772023acf3f2f8be087808aff35d1d4b47fdc7
FeromonDE <FeromonDE@users.noreply.github.com>
2026-09-25 11:28:41 +0200

    Sync storage cache switching with Bambu Studio

--- src/slic3r/GUI/Printer/PrinterFileSystem.cpp
+++ src/slic3r/GUI/Printer/PrinterFileSystem.cpp
@@ -118,11 +118,14 @@ void PrinterFileSystem::SetFileType(FileType type, std::string const &storage)
 {
     if (m_file_type == type && m_file_storage == storage)
         return;
+    bool storage_only_changed = (m_file_type == type && m_file_storage != storage);
     SelectAll(false);
     assert(m_file_list_cache[std::make_pair(m_file_type, m_file_storage)].empty());
     m_file_list.swap(m_file_list_cache[{m_file_type, m_file_storage}]);
     std::swap(m_file_type, type);
     m_file_storage = storage;
+    if (storage_only_changed)
+        m_file_list_cache[{m_file_type, m_file_storage}].clear();
     m_file_list.swap(m_file_list_cache[{m_file_type, m_file_storage}]);
     m_lock_start = m_lock_end = 0;
     BuildGroups();
